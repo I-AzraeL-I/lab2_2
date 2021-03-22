1c2e3dca8bdb3b801aab4878d21bcc3f6a6b17b3
Kacper.Gajewski <222431@edu.p.lodz.pl>
2021-03-22 18:22:46 +0100

    Implemented test no.1 and added initializer

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,14 +1,39 @@
 package edu.iis.mto.similarity;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
+import edu.iis.mto.searcher.SequenceSearcher;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 class SimilarityFinderTest {
 
-    @Test
-    void test() {
-        fail("Not yet implemented");
+    private final SequenceSearcher sequenceSearcher = null;
+    private final SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcher);
+    private int[] arr1;
+    private int[] arr2;
+
+    @BeforeEach
+    void initialize() {
+        final int MAX_RANGE = 100;
+        final int MIN_RANGE = 1;
+        arr1 = new int[ThreadLocalRandom.current().nextInt(MIN_RANGE, MAX_RANGE)];
+        arr2 = new int[ThreadLocalRandom.current().nextInt(MIN_RANGE, MAX_RANGE)];
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = ThreadLocalRandom.current().nextInt(MIN_RANGE, MAX_RANGE);
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            arr2[i] = ThreadLocalRandom.current().nextInt(MIN_RANGE, MAX_RANGE);
+        }
     }
 
+    @Test
+    void resultShouldBeEqualTo1() {
+        int[] arr1_1 = {};
+        int[] arr2_1 = {};
+        double result = similarityFinder.calculateJackardSimilarity(arr1_1, arr2_1);
+        assertEquals(1.0d, result);
+    }
 }
